c7c2c43cf485cc3f7d9c85f41844c86e08511a52
mflamanna <lamanna.mflorencia@gmail.com>
2022-06-22 14:55:42 +0200

    primer repositorio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <div className="header-bg"></div>
+        <img
+          className="header-logo"
+          src=""
+          alt="pokemon-logo"
+        ></img>
+        <nav className="header-search_bar">
+          <input type="text" name="search" placeholder="Search"></input>
+          <button type="submit">GO</button>
+        </nav>
       </header>
+      <main className="list-pokemons">
+        <article className="list-pokemons-item">
+          <div className="list-pokemons-item-content">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+              alt="pokemon-1"
+            ></img>
+            <h3>
+              Bulbasaur
+              <span>gras</span>
+            </h3>
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
